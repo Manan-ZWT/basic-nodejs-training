0c3241ab06f220f00c66d7531b67749576ac3bb9
Manan-ZWT <manan.patel@zealousweb.com>
2025-01-21 16:49:59 +0530

    Countdowntimer complete

---
+++ Day 1/countdowntimer.js
@@ -0,0 +1,13 @@
+const process = require("process");
+
+let end_time = parseInt(process.argv[2]);
+
+const count = setInterval(() =>{
+  if (end_time > 0) {
+    console.log(`Remaining time: ${end_time}s`);
+    end_time --;
+  } else {
+    console.log("Time's up");
+    clearInterval(count);
+  }
+},1000);

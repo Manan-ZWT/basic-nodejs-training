b8708c1f589fe1fb1886615a4f60740406abf798
Manan-ZWT <manan.patel@zealousweb.com>
2025-01-21 18:06:14 +0530

    Updating files

--- Day 1/reminder.js
+++ Day 1/reminder.js
@@ -1,10 +1,13 @@
 let reminder_time = parseInt(process.argv[2]);
 let reminder_task = process.argv[3];
 
-console.log(`A reminder has been set: ${reminder_task} after ${reminder_time}s`)
-const reminder=setTimeout(()=>{
-    console.log(
+console.log(
+  `A reminder has been set: ${reminder_task} after ${reminder_time}s`
+);
+const reminder = setTimeout(() => {
+  console.log(
     `     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
                                                               Reminder to ${reminder_task}                                                                         
-     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`)
-},reminder_time*1000);
+     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`
+  );
+}, reminder_time * 1000);

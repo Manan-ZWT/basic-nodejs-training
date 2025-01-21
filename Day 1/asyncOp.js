7162509fc90c7bef30c5bf9058ce3148edfac4b2
Manan-ZWT <manan.patel@zealousweb.com>
2025-01-21 18:23:27 +0530

    Completed async operation program

---
+++ Day 1/asyncOp.js
@@ -0,0 +1,30 @@
+let success_bool = parseInt(process.argv[2]);
+let success_value = true;
+
+if (success_bool == 1) {
+  success_value = true;
+} else if (success_bool == 0) {
+  success_value = false;
+} else {
+  return;
+}
+
+function fetchData(success) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (success) {
+        resolve("Data fetched successfully!");
+      } else {
+        reject("Error: Failed to fetch data.");
+      }
+    }, 2000);
+  });
+}
+
+fetchData(success_bool)
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((error) => {
+    console.error(error);
+  });

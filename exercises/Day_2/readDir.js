f12039c1c5e565adfc3b3de8df6864c3995c2c1c
Manan-ZWT <manan.patel@zealousweb.com>
2025-01-22 13:18:45 +0530

    Created readDir.js

--- exercises/Day_2/readDir.js
+++ exercises/Day_2/readDir.js
@@ -1,8 +1,17 @@
 const fs = require("fs");
 
-const dirpath=`./Day 1`;
+
+const dirpath=`..\\Day_1`;
 
 fs.readdir(dirpath,(err,files)=>{
-    
-}
-
+    try{
+        console.log(`List of files in ${dirpath} :`)
+        files.forEach((file)=>{
+            console.log(`\n${file}`);
+        });
+    }
+    catch(err){
+        // console.log(err);
+        console.log(`no file found in the directory`);
+    }
+});

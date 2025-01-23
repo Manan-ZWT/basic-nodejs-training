4f09761f7816839b4d4c6194e76c61b88f9ba36e
Manan-ZWT <manan.patel@zealousweb.com>
2025-01-23 18:55:36 +0530

    Completed the basic fucntionalities of express server

--- exercises/Day_3/index.js
+++ exercises/Day_3/index.js
@@ -1,23 +1,77 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-
-import { users } from "./constant";
+import { users } from "./constant.js";
 
 const app = express();
-const port=process.env.D3_APP_PORT;
+const port = process.env.D3_APP_PORT;
 
-app.get("/",(req, res)=>{
-    console.log("User connected to the server");
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Welcome to the User Management API!");
+app.use(express.json());
+app.get("/", (req, res) => {
+  console.log("User connected to the server");
+  res.status(200).send(`Welcome to the User Management API!`);
 });
 
-app.listen(port,(err)=>{
-if(err){
-    console.error("Erro occured: Server not started")
-}
-else{
+app.get("/users", (req, res) => {
+  if (users.length === 0) {
+    res.status(404).send("No users found");
+  } else {
+    users;
+    res.status(200).json(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  let userId = parseInt(req.params.id);
+  let user = users.find((u) => u.id === userId);
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).send("User not found!");
+  }
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  let { name, email, age, role, isActive } = req.body;
+  let id = users.length + 1;
+  users.push({ id, name, email, age, role, isActive });
+  res.status(200).send("User has been succesfully added");
+});
+
+app.patch("/users/:id", (req, res) => {
+  console.log(req.body);
+  let { name, email, age, role, isActive } = req.body;
+  let userId = parseInt(req.params.id);
+  let user = users.find((u) => u.id === userId);
+  if (user) {
+    user.name = name;
+    user, (email = email);
+    user.age = age;
+    user.role = role;
+    user.isActive = isActive;
+    res.status(200).send("User has been succesfully updated");
+  } else {
+    res.status(404).send("User not Found!");
+  }
+});
+
+app.delete("/users/:id", (req, res) => {
+  let userId = parseInt(req.params.id);
+  let userIndex = users.findIndex((u) => u.id === userId);
+  if (userIndex !== -1) {
+    users.splice(userIndex, 1);
+    res.status(200).json("User has been succesfully deleted");
+  } else {
+    res.status(404).send("User not Found!");
+  }
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Error occured: Server not started");
+  } else {
     console.log(`Server started at port: ${port}`);
-}
+  }
 });

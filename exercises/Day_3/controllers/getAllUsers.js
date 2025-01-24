ac09195a26639c1033ce227cee3c1b0f307df644
Manan-ZWT <manan.patel@zealousweb.com>
2025-01-24 18:19:10 +0530

    Added the validations in filters

--- exercises/Day_3/controllers/getAllUsers.js
+++ exercises/Day_3/controllers/getAllUsers.js
@@ -1,6 +1,7 @@
 import { users } from "../../../constant.js";
 
 export const getAllUsers = (req, res) => {
+  const validRoles = ["Admin", "User"];
   if (users.length === 0) {
     res.status(404).json({ error: "No users found" });
   } else if (req.query) {
@@ -8,20 +9,36 @@ export const getAllUsers = (req, res) => {
     let filteredUsers = users;
 
     if (role) {
-      filteredUsers = filteredUsers.filter((user) => user.role === role);
+      if (validRoles.includes(role)) {
+        filteredUsers = filteredUsers.filter((user) => user.role === role);
+      } else {
+        return res
+          .status(406)
+          .json({ error: `Not a valid role,Enter a valid role` });
+      }
     }
 
     if (isActive !== undefined) {
-      const isActiveBool = isActive === "true";
-      filteredUsers = filteredUsers.filter(
-        (user) => user.isActive === isActiveBool
-      );
+      const isActiveBool = isActive.toLowerCase() === "true";
+      if (isActive === "true" || isActive === "false") {
+        filteredUsers = filteredUsers.filter(
+          (user) => user.isActive === isActiveBool
+        );
+      } else {
+        return res
+          .status(406)
+          .json({ error: "Not a valid input, must be a Boolean" });
+      }
     }
 
     if (ageGt) {
       const ageLimit = parseInt(ageGt);
-      if (Number.isInteger(ageLimit)) {
+      if (Number.isInteger(ageLimit) && ageLimit >= 1) {
         filteredUsers = filteredUsers.filter((user) => user.age > ageLimit);
+      } else {
+        return res
+          .status(406)
+          .json({ error: `Age can only be a positive integer` });
       }
     }
 
